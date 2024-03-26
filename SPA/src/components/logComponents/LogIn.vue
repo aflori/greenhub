cc10de86159ba4acb32b262e136d9f73ff25992f
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-26 13:49:04 +0100

    created redirection to login page

---
+++ SPA/src/components/logComponents/LogIn.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import LogIn from "@/components/icons/Sign_in_squre.vue";
+    import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+    <router-link :to="{name: 'log_in'}" class="my-auto">
+        <LogIn  class="h-16 w-16" />
+    </ router-link>
+</template>
--- SPA/src/components/molecules/LogDatas.vue
+++ SPA/src/components/molecules/LogDatas.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import LogIn from "@/components/icons/Sign_in_squre.vue";
+    import LogIn from "@/components/logComponents/LogIn.vue";
     import LogOut from "@/components/icons/Sign_out_squre.vue";
 
     import { useLoggingStore } from "@/stores/loggin.js";
@@ -8,6 +8,6 @@
 </script>
 
 <template>
-    <LogIn class="h-16 w-16 my-auto" v-if="myStore.isLogged" />
-    <LogOut class="h-14 w-14 my-auto" v-else />
+    <LogOut class="h-14 w-14 my-auto" v-if="myStore.isLogged" />
+    <LogIn v-else />
 </template>
