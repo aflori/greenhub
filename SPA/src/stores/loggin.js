cbbb3d576872e57577d7b1d797e9b1ab259fdc80
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-26 15:59:55 +0100

    getting csrf token request

--- SPA/src/stores/loggin.js
+++ SPA/src/stores/loggin.js
@@ -1,18 +1,27 @@
 import { defineStore } from 'pinia';
+import axios from 'axios';
 
+const baseUrl = "http://127.0.0.1:8000/"
 
 export const useLoggingStore = defineStore('logging', {
     state: () => ({
         isLogged: false,
+        hasSession: false,
     }),
     getters: {
 
     },
     actions: {
         getSession() {
-            axios.get("/sanctum/csrf-cookie").then( response => {
-                // something
+            const url = baseUrl + "sanctum/csrf-cookie"
+            axios.get(url).then( response => {
             });
+        },
+
+        logIn(email, password) {
+            if(! this.hasSession) {
+                this.getSession()
+            }
         }
     }
 });
