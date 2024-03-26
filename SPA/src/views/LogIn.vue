c7fdabd03c69a3b1c0c64daef457e58297d93270
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-26 14:15:35 +0100

    redirect if someone connected try to connect when logged

--- SPA/src/views/LogIn.vue
+++ SPA/src/views/LogIn.vue
@@ -1,4 +1,14 @@
 <script setup>
+    import { useLoggingStore } from "@/stores/loggin.js"
+    import { useRouter } from 'vue-router'
+
+    const router = useRouter()
+
+
+    const logData = useLoggingStore();
+    if (logData.isLogged) {
+        router.push({name: "home"})
+    }
 </script>
 
 <template>
