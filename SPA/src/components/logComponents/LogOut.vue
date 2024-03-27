b5ee7149773da46c7f26c0163078c9b460c90c45
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-27 14:28:00 +0100

    moving from option store to setup store

--- SPA/src/components/logComponents/LogOut.vue
+++ SPA/src/components/logComponents/LogOut.vue
@@ -1,10 +1,14 @@
 <script setup>
     import LogOut from "@/components/icons/Sign_out_squre.vue";
     import { RouterLink } from 'vue-router';
+
+    import { useLoggingStore } from "@/stores/loggin.js";
+    const logConnectionStore = useLoggingStore();
+
 </script>
 
 <template>
     <router-link :to="{name: 'log_in'}" class="my-auto">
-        <LogOut class="h-14 w-14"/>
+        <LogOut class="h-14 w-14" @click="logConnectionStore.logout"/>
     </ router-link>
 </template>
--- SPA/src/stores/loggin.js
+++ SPA/src/stores/loggin.js
@@ -1,38 +1,40 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
+import { ref } from 'vue';
 
 const baseUrl = "http://localhost:8000/"
 
-export const useLoggingStore = defineStore('logging', {
-    state: () => ({
-        isLogged: false,
-        name: "",
-        hasSession: false,
-    }),
-    getters: {
-    },
-    actions: {
-        async getSession() {
-            const url = baseUrl + "sanctum/csrf-cookie";
-            await axios.get(url);
-            this.hasSession = true;
-        },
+export const useLoggingStore = defineStore('logging', () => {
+    const isLogged = ref(false);
+    const name = ref("");
+    const hasSession = ref(false);
 
-        async logIn(email, password) {
-            if(! this.hasSession ){
-                await this.getSession()
-            }
-
-            const url = baseUrl + "login";
-
-            await axios.post(url, {
-                'email': email,
-                'password': password,
-            })
-
-            this.name = email;
-            this.isLogged = true;
-
-        }
+    async function getSession() {
+        const url = baseUrl + "sanctum/csrf-cookie";
+        await axios.get(url);
+        hasSession.value = true;
     }
-});
+
+    async function logIn(email, password) {
+        if(! hasSession.value ){
+            await getSession()
+        }
+
+        const url = baseUrl + "login";
+
+        await axios.post(url, {
+            'email': email,
+            'password': password,
+        })
+
+        name.value = email;
+        isLogged.value = true;
+    }
+    function logout(){
+        const url = baseUrl + "logout";
+        axios.post(url).then((_) =>{
+            isLogged.value = false;
+        })
+    }
+    return {isLogged, name, getSession, logIn, logout};
+})
