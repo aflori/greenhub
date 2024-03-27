d9774ef0257b27bd96a54887478637a85c8d55f3
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-27 12:15:55 +0100

    log in fix and update

--- SPA/src/main.js
+++ SPA/src/main.js
@@ -4,6 +4,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true;
+axios.defaults.withXSRFToken = true;
+axios.defaults.headers.common["Accept"] = "application/json";
 
 const app = createApp(App)
 
--- SPA/src/stores/loggin.js
+++ SPA/src/stores/loggin.js
@@ -1,27 +1,38 @@
 import { defineStore } from 'pinia';
 import axios from 'axios';
 
-const baseUrl = "http://127.0.0.1:8000/"
+const baseUrl = "http://localhost:8000/"
 
 export const useLoggingStore = defineStore('logging', {
     state: () => ({
         isLogged: false,
+        name: "",
         hasSession: false,
     }),
     getters: {
-
     },
     actions: {
-        getSession() {
-            const url = baseUrl + "sanctum/csrf-cookie"
-            axios.get(url).then( response => {
-            });
+        async getSession() {
+            const url = baseUrl + "sanctum/csrf-cookie";
+            await axios.get(url);
+            this.hasSession = true;
         },
 
-        logIn(email, password) {
-            if(! this.hasSession) {
-                this.getSession()
+        async logIn(email, password) {
+            if(! this.hasSession ){
+                await this.getSession()
             }
+
+            const url = baseUrl + "login";
+
+            await axios.post(url, {
+                'email': email,
+                'password': password,
+            })
+
+            this.name = email;
+            this.isLogged = true;
+
         }
     }
 });
