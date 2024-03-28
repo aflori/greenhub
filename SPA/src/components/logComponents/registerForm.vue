de6117330942a9db76cac8c8d2d1335ac4553add
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-28 11:28:45 +0100

    register form field

--- SPA/src/components/logComponents/registerForm.vue
+++ SPA/src/components/logComponents/registerForm.vue
@@ -12,20 +12,49 @@
     */
     const logData = useLoggingStore();
 
-    /*
-    const email = ref("");
-    const password = ref("password");
-    const hasErrorInLogging = ref(false);
-    const isWaitingForAnswer = ref(false);
 
-    }
-    */
+    const firstName = ref("");
+    const lastName = ref("");
+    const pseudoname = ref("");
+    const email = ref("");
+    const emailConfirmed = ref("");
+    const password = ref("");
+    const passwordConfirmed = ref("");
+
+
 </script>
 
 <template>
-        <form class="max-w-80">
-            ceci est le form de connection
-        </form>
+    <form class="max-w-96">
+        <label class="input input-bordered flex items-center gap-2">
+            prénom:
+            <input type="text" class="grow" placeholder="Jhon" v-model="firstName" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            nom:
+            <input type="text" class="grow" placeholder="Doe" v-model="lastName" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            pseudoname:
+            <input type="text" class="grow" placeholder="doigt" v-model="pseudoname" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            mail:
+            <input type="email" class="grow" placeholder="exemple@exemple.com" v-model="email" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            confirmer l'adresse mail:
+            <input type="email" class="grow" placeholder="exemple@exemple.com" v-model="emailConfirme" @paste.prevent />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            mot de passe:
+            <input type="password" class="grow" v-model="password" />
+        </label>
+        <label class="input input-bordered flex items-center gap-2">
+            confirmer le mail:
+            <input type="password" class="grow" v-model="passwordConfirmed" @paste.prevent />
+        </label>
+    </form>
 </template>
 
 <style scoped>
