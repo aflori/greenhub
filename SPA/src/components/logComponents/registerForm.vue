7958043c1e92c7fb4d2992a3c69de77a014e6db9
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-28 10:05:22 +0100

    added form change on login page

---
+++ SPA/src/components/logComponents/registerForm.vue
@@ -0,0 +1,36 @@
+<script setup>
+    import { useLoggingStore } from "@/stores/loggin.js"
+    // import { useRouter } from 'vue-router'
+    import { ref } from 'vue'
+
+    /*
+    const router = useRouter()
+
+    function returnToHome() {
+        router.push({name: "home"})
+    }
+    */
+    const logData = useLoggingStore();
+
+    /*
+    const email = ref("");
+    const password = ref("password");
+    const hasErrorInLogging = ref(false);
+    const isWaitingForAnswer = ref(false);
+
+    }
+    */
+</script>
+
+<template>
+        <form class="max-w-80">
+            ceci est le form de connection
+        </form>
+</template>
+
+<style scoped>
+.warning {
+    color: #F00;
+    font-size: 2rem;
+}
+</style>
--- SPA/src/views/LogIn.vue
+++ SPA/src/views/LogIn.vue
@@ -1,15 +1,16 @@
 <script setup>
-    //import { useLoggingStore } from "@/stores/loggin.js"
-    //import { useRouter } from 'vue-router'
-    //import { ref } from 'vue'
-
     import LogInForm from '@/components/logComponents/logInForm.vue'
+    import RegisterForm from '@/components/logComponents/registerForm.vue'
+    import { ref } from 'vue'
+
+    const hasAnAcount = ref(true);
 
 </script>
 
 <template>
     <main>
-        <LogInForm />
+        <LogInForm v-if="hasAnAcount" />
+        <RegisterForm v-else />
    </main>
 </template>
 
