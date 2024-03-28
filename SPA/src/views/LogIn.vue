6874fcd9b9e3ba274f1bca55d195423217fd30f5
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-28 10:16:19 +0100

    added button to change form

--- SPA/src/views/LogIn.vue
+++ SPA/src/views/LogIn.vue
@@ -3,14 +3,40 @@
     import RegisterForm from '@/components/logComponents/registerForm.vue'
     import { ref } from 'vue'
 
-    const hasAnAcount = ref(true);
+    const textToRegister = "créer un compte"
+    const textToLogIn = "Se connecter"
 
+    const hasAnAcount = ref(true);
+    const textFormSwitch = ref(textToRegister)
+
+
+    function changeForm() {
+        if (hasAnAcount.value) {
+            hasAnAcount.value = false;
+            textFormSwitch.value = textToLogIn
+        } else {
+            hasAnAcount.value = true;
+            textFormSwitch.value  = textToRegister;
+        }
+    }
 </script>
 
 <template>
     <main>
         <LogInForm v-if="hasAnAcount" />
         <RegisterForm v-else />
+
+        <a @click.prevent="changeForm"> {{ textFormSwitch }}</a>
    </main>
 </template>
 
+<style scoped>
+a:hover {
+    cursor: pointer;
+}
+
+a {
+    text-decoration: underline;
+    color: blue;
+}
+</style>
