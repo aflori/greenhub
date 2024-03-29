f7c160b54978ddb2e5e52d5a55db645c37ed1729
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-29 10:19:10 +0100

    Filter components added

---
+++ SPA/src/components/products/filterField.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <p> filter world! </p>
+    </div>
+</template>
+
+<style scoped>
+</style>
--- SPA/src/views/Products.vue
+++ SPA/src/views/Products.vue
@@ -1,5 +1,6 @@
 <script setup>
     import BasicProductCard from "@/components/molecules/BasicProductCard.vue";
+    import FilterCard from "@/components/products/filterField.vue"
     import { useProductListStore } from "@/stores/listProduct.js";
 
     const list = useProductListStore();
@@ -7,9 +8,10 @@
 </script>
 
 <template>
-    <main class="flex flex-row flex-wrap justify-evenly">
-
-        <BasicProductCard :product="product" v-for="product in list.products" class="my-2"/>
-
+    <main class="flex flex-row">
+        <FilterCard class="w-1/12"/>
+        <div class="flex flex-row flex-wrap justify-evenly w-11/12">
+            <BasicProductCard :product="product" v-for="product in list.products" class="my-2"/>
+        </div>
     </main>
 </template>
