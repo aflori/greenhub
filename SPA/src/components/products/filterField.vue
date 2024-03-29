228d029fad7fd60001fb63c545ee0b1f0170e67b
aflori <aurelien.flori@le-campus-numerique.fr>
2024-03-29 11:00:37 +0100

    added search icon

--- SPA/src/components/products/filterField.vue
+++ SPA/src/components/products/filterField.vue
@@ -1,11 +1,15 @@
 <script setup>
+    import LogoSearch from "@/components/icons/Search_alt_light.vue";
 </script>
 
 <template>
     <div>
-        <p> filter world! </p>
+        <LogoSearch class="black" /> <p> filter world! </p>
     </div>
 </template>
 
 <style scoped>
+.black {
+    color: black;
+}
 </style>
