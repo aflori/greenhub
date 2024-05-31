70799cc86a1c5da4c1d4d72afc86ccac0fd21ba8
aflori <f.aurelien@yahoo.fr>
2024-05-31 16:33:44 +0200

    front: end of validation refacto in cart adress step

--- SPA/src/components/CartComponents/CartAdressStep.vue
+++ SPA/src/components/CartComponents/CartAdressStep.vue
@@ -1,23 +1,34 @@
 <script setup>
 import { useFormStore } from '@/stores/form.js'
 import FormField from './CartFormField.vue'
+import { ref } from 'vue' 
 
 const emit = defineEmits(['next-step', 'prev-step'])
 
 const formStore = useFormStore()
 const formName = 'adressDelivery'
 const formData = formStore[formName]
+const errors = ref([])
+
 
 function goToNextStepIfDataValid() {
-  const errors = formStore.getInvalidOutputList()
-  if(errors.length === 0) {
+  const validationsErrors = formStore.getInvalidOutputList()
+  
+  if(validationsErrors.length === 0) {
     emit('next-step')
+    return
   }
+
+  errors.value = validationsErrors
 }
 </script>
 
 <template>
   <form>
+    <p v-if="errors.length > 0" class="error">
+      Les champs du formulaire sont invalides.
+      Veuillez corriger les champs <span v-for="e in errors"> {{ e }}, </span> 
+    </p>
     <FormField :label="label" :form="formName" v-for="(field, label) in formData" :key="label" />
     <button
       type="submit"
@@ -29,3 +40,9 @@ function goToNextStepIfDataValid() {
     <button type="reset" class="btn" @click.prevent="$emit('prev-step')">retour</button>
   </form>
 </template>
+
+<style scoped>
+.error {
+  color: #F00;
+}
+</style>
